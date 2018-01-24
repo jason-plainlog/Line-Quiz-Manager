b916384f8bcad8e2a571c373a879c32030fab0b8
Jason Hsieh <jason.plainlog@gmail.com>
2018-01-24 20:27:14 +0800

    Bug fixed

--- app.rb
+++ app.rb
@@ -11,8 +11,8 @@ class MyApp < Sinatra::Application
 
 	post '/webhook' do
 		client = Line::Bot::Client.new { |config|
-			config.channel_secret = 'cb28a06983cf5e153656107c83f82ee7',
-			config.channer_token = 'xnich5DiM9M93HylaU+N8zLX1A5KwVCdgawIb1Z0YYAxIYM7S/+Hnrv6ZG3woopl8PYZ3RrUUT50f6Kqh8IdJUow9QPHUEd2dVv6eJPM8n1XJ3Cp3XCbb06ws4zei7HBX9mY06KYOgrkHlK67QfNHQdB04t89/1O/w1cDnyilFU='
+			config.channel_secret = 'cb28a06983cf5e153656107c83f82ee7'
+			config.channel_token = 'xnich5DiM9M93HylaU+N8zLX1A5KwVCdgawIb1Z0YYAxIYM7S/+Hnrv6ZG3woopl8PYZ3RrUUT50f6Kqh8IdJUow9QPHUEd2dVv6eJPM8n1XJ3Cp3XCbb06ws4zei7HBX9mY06KYOgrkHlK67QfNHQdB04t89/1O/w1cDnyilFU='
 		}
 
 		reply_token = params['events'][0]['replyToken']
