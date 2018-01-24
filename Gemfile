c458df9640696f6bdfcc08c3317865be8f3b0f3d
Jason Hsieh <jason.plainlog@gmail.com>
2018-01-24 20:20:13 +0800

    Reply Test

--- Gemfile
+++ Gemfile
@@ -11,3 +11,6 @@ gem "sinatra", "~> 2.0"
 
 # Added at 2018-01-24 18:49:46 +0800 by jason:
 gem "redis", "~> 4.0"
+
+# Added at 2018-01-24 20:14:38 +0800 by jason:
+gem "line-bot-api", "~> 1.2"
--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'line/bot'
 require 'redis'
 
 class MyApp < Sinatra::Application
@@ -9,6 +10,20 @@ class MyApp < Sinatra::Application
 	end
 
 	post '/webhook' do
+		client = Line::Bot::Client.new { |config|
+			config.channel_secret = 'cb28a06983cf5e153656107c83f82ee7'
+			config.channer_token = 'xnich5DiM9M93HylaU+N8zLX1A5KwVCdgawIb1Z0YYAxIYM7S/+Hnrv6ZG3woopl8PYZ3RrUUT50f6Kqh8IdJUow9QPHUEd2dVv6eJPM8n1XJ3Cp3XCbb06ws4zei7HBX9mY06KYOgrkHlK67QfNHQdB04t89/1O/w1cDnyilFU='
+		}
+
+		reply_token = params['events'][0]['replyToken']
+
+		message = {
+			type: 'text',
+			text: '已讀'
+		}
+
+		response = client.reply_message(reply_token, message)
+
 		status 200
 	end
 end
